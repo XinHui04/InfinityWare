7ef98ec63f3dd8ead8813236d89779b1799ad444
XinHui04 <lxinhui0401@gmail.com>
2025-03-09 16:06:22 +0800

    user module

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
+
+        while (running) {
+            System.out.println("\nComputer Retail Management System:");
+            System.out.println("1. Register");
+            System.out.println("2. Login");
+            System.out.println("3. Exit");
+            System.out.print("Choose an option: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // Consume newline
+
+            switch (choice) {
+                case 1:
+                    UserModule.registerUser();
+                    break;
+                case 2:
+                    UserModule.loginUser();
+                case 3:
+                    running = false;
+                    System.out.println("Exiting system...");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
+        scanner.close();
+    }
+}
